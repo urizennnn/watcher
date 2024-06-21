5676ebb2f854b869b490988a63889d4cc7201332
urizennnn <igamerryt@gmail.com>
2024-06-21 22:51:54 +0100

    chore:added uwatch system

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -1,7 +1,7 @@
 use crate::{parser::parser::parse, watchlib::watch::start_watch};
 use clap::{self, Arg, ArgAction, Command};
 // use once_cell::sync::Lazy;
-use std::env;
+use std::{env, path::PathBuf};
 
 // static CONFIG: Lazy<Config> = Lazy::new(|| parse().expect("Failed to parse Config file"));
 
@@ -9,6 +9,7 @@ pub fn cli() {
     let matches = Command::new("Watcher")
         .version("0.0.1")
         .author("urizen")
+        .allow_missing_positional(true)
         .about("A file watcher used to restart applications on changes.")
         .arg(
             Arg::new("dev")
@@ -27,17 +28,20 @@ pub fn cli() {
         .arg(Arg::new("directory").index(1).required(false))
         .about("Directory to watch")
         .get_matches();
+
     let config = parse().unwrap().name;
     if matches.get_flag("dev") {
         println!("{}", config);
         return;
     }
 
+    let exclude_dir = vec![PathBuf::from("target/"), PathBuf::from("src/")];
+
     if let Some(directory) = matches.get_one::<String>("directory") {
-        start_watch(directory);
+        start_watch(directory, exclude_dir);
     } else {
         if let Ok(cwd) = env::current_dir() {
-            start_watch(cwd.to_str().unwrap())
+            start_watch(cwd.to_str().unwrap(), exclude_dir)
         } else {
             eprintln!("Failed to get current working directory");
         }
--- watchlib/watch.rs
+++ watchlib/watch.rs
@@ -1,38 +1,38 @@
 use colored::Colorize;
 use notify::{Config, Event, EventKind, PollWatcher, RecursiveMode, Watcher};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
-fn build_watch_path(directory: &str) -> String {
+pub fn build_watch_path(directory: &str) -> String {
     directory.to_string()
 }
 
-fn handle_event(event: notify::Result<Event>, exclude_dir: &Path) {
+fn handle_event(event: notify::Result<Event>, exclude_dir: &[PathBuf]) {
     match event {
         Ok(Event { kind, paths, .. }) => {
             for path in paths {
-                if !path.starts_with(exclude_dir) {
+                if exclude_dir
+                    .iter()
+                    .any(|exclude_path| path.starts_with(exclude_path))
+                {
+                    println!(
+                        "{}",
+                        "Excluded directory got modified, not restarting".red(),
+                    );
+                } else {
                     match kind {
                         EventKind::Modify(notify::event::ModifyKind::Data(_)) => {
                             println!("Data modified: {:?}", path);
                         }
-                        EventKind::Create(_) => {
+                        EventKind::Create(notify::event::CreateKind::File) => {
                             println!("File created: {:?}", path);
                         }
                         EventKind::Remove(_) => {
                             println!("File removed: {:?}", path);
                         }
-                        EventKind::Access(_) => {
-                            println!("File accessed: {:?}", path);
-                        }
                         _ => {}
                     }
-                } else {
-                    println!(
-                        "{}",
-                        "Excluded directory got modiified,not restarting".red(),
-                    );
                 }
             }
         }
@@ -40,43 +40,43 @@ fn handle_event(event: notify::Result<Event>, exclude_dir: &Path) {
     }
 }
 
-pub fn start_watch(directory: &str) {
+pub fn start_watch(directory: &str, mut exclude_dir: Vec<PathBuf>) {
     let (tx, rx) = channel();
+
     let mut watcher = PollWatcher::new(
         tx,
-        Config::default().with_poll_interval(Duration::from_millis(0)),
+        Config::default().with_poll_interval(Duration::from_millis(1000)),
     )
     .expect("Failed to create watcher");
 
-    let watch_dir = build_watch_path(directory);
-    let watch_path = Path::new(&watch_dir);
+    let watch_path: PathBuf = Path::new(directory).into();
     if !watch_path.exists() {
-        eprintln!("Directory '{}' doesn't exist", watch_dir.red());
+        eprintln!("Directory '{}' doesn't exist", watch_path.display());
         return;
     }
 
-    watcher
-        .watch(watch_path, RecursiveMode::Recursive)
-        .expect("Failed to watch directory");
+    exclude_dir = exclude_dir
+        .into_iter()
+        .map(|ex| watch_path.join(ex))
+        .collect();
 
-    let exclude_dir = watch_path.join("target");
+    watcher
+        .watch(&watch_path, RecursiveMode::Recursive)
+        .expect("Failed to watch directory");
 
     println!(
         "{}",
         format!(
-            "Watching directory '{}' and excluding '{}'",
-            watch_dir,
-            exclude_dir.display()
+            "Watching directory '{}'",
+            watch_path.display().to_string().green()
         )
-        .green()
     );
-
     loop {
         match rx.recv() {
             Ok(event) => handle_event(event, &exclude_dir),
             Err(e) => {
                 eprintln!("Watch error: {:?}", e);
-                return;
+                break;
             }
         }
     }
